17bc7c09f99b627c8ac6cb1a8b214847c8710ad1
QioNight <qionight@yeah.net>
2020-01-04 12:16:41 +0800

    react_len

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="App-name">Simulation Chat Room</p>
+        </header>
+        <div className="App-body">body</div>
+        <footer>footer</footer>
+      </div>
+    );
+  }
 }
 
-export default App;
+export { App };
