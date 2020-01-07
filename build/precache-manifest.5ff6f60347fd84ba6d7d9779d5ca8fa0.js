8b9466e5b5f5792d30db6555687dce065937f30f
QioNight <qionight@yeah.net>
2020-01-07 17:13:49 +0800

    test gh-page

---
+++ build/precache-manifest.5ff6f60347fd84ba6d7d9779d5ca8fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83800a528fb53788f45aed0d0d885033",
+    "url": "./index.html"
+  },
+  {
+    "revision": "12929558b5832656a702",
+    "url": "./static/css/main.550a1e3f.chunk.css"
+  },
+  {
+    "revision": "c4311641ae55255b07d1",
+    "url": "./static/js/2.3606588e.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "./static/js/2.3606588e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12929558b5832656a702",
+    "url": "./static/js/main.6cde68e5.chunk.js"
+  },
+  {
+    "revision": "608d22ca2fa76f11d044",
+    "url": "./static/js/runtime-main.34582d8f.js"
+  }
+]);
